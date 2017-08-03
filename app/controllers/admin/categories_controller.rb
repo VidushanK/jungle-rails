ea9794fdf8f27e4403a4b4115e8cf9a9a990efde
Vidushan Kohiladas <vidushan@Vidushans-MacBook-Air.local>
2017-08-03 11:03:39 -0700

    initial commit

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,35 @@
+# create a new  categories_controller in admins folder?
+ #admin/category#create
+ #
+# implement def new and def index in categories controller
+# create views/admin/categories directory
+# create index.html.erb
+  # textarea to input category names
+  # contains a button that submits category name
+  # option to add products into the category
+
+
+class Admin::CategoriesController < ApplicationController
+  def new
+    @category = Category.new
+  end
+
+  def index
+    @categories = Category.order(id: :desc).all
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+  def category_params
+    params.require(:category).permit(
+      :name,
+    )
+  end
+end
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,3 +1,14 @@
+# create a new  categories_controller in admins folder?
+ #admin/category#create
+ #
+# implement def new and def index in categories controller
+# create views/admin/categories directory
+# create index.html.erb
+  # textarea to input category names
+  # contains a button that submits category name
+  # option to add products into the category
+
+
 class Admin::ProductsController < ApplicationController
 
   def index
