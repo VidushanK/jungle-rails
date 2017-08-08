904ee0150dd3a5ed22b0bb54e188490b499fb21a
Vidushan Kohiladas <vidushan@Vidushans-MacBook-Air.local>
2017-08-08 11:40:50 -0700

    user validataions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :reviews
+  validates :full_name, presence: true
   validates :email, :presence => true,  :uniqueness => { :case_sensitive => false }, :format => {:with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i}
+  validates :password, :length => {:within => 6..40}
+  validates :password_confirmation, :length => {:within => 6..40}
+
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,49 @@
 require 'rails_helper'
 
+
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    @user = User.new(full_name: "test", email:"vidushan@test.com", password: "1234567", password_confirmation: "1234567")
+  end
+
+  describe 'Validations' do
+
+    it "Check if full name is blank" do
+      @user.full_name = ''
+      expect(@user.valid?).to be false
+      end
+
+    it "Check if email is blank" do
+      @user.email = ''
+      expect(@user.valid?).to be false
+    end
+
+    it "Check if password is present" do
+      @user.password = nil
+      expect(@user.valid?).to be false
+    end
+
+    it "Check if password_confirmation is blank" do
+      @user.password_confirmation = ''
+      expect(@user.valid?).to be false
+    end
+
+    it "Check if email has required input" do
+      @user.email = nil
+      expect(@user.valid?).to be false
+    end
+
+    it "should be matching password and password_confirmation field" do
+      # @user.password = "123456"
+      # @user.password_confirmation = "654321"
+      expect(@user.password_confirmation).to eq(@user.password)
+    end
+
+    it "should not have a password longer or equal to 6 characters" do
+      # @user.password = "123"
+      expect(@user.password.length).to be >= 6
+    end
+  end
+  
+
 end
